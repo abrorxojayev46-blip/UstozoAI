c9e8150c1d0cba69d035faca39cdb9b837e7f050
abrorxojayev46 <abrorxojayev46@gmail.com>
2026-04-10 10:48:24 -0700

    Update push.bat with correct GitHub URL

--- push.bat
+++ push.bat
@@ -4,7 +4,8 @@ git config user.email "abrorxojayev46@gmail.com"
 git config user.name "abrorxojayev46"
 git add .
 git commit -m "Ustoz AI - Uzbek AI tutor app"
-git remote add origin https://github.com/abrorxojayev46/UstozoAI.git
+git remote remove origin
+git remote add origin https://github.com/abrorxojayev46-blip/UstozoAI.git
 git branch -M main
 git push -u origin main
 pause
